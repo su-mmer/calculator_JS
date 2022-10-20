a24a9bc7fdd471278d4a954031079a384d6eaf5e
su-mmer <heekim987@gmail.com>
2022-10-21 00:53:00 +0900

    add function 'add'

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,15 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-currentResult = currentResult + 10 * 3;
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
 
-outputResult(currentResult, '');
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`;  // 내가 입력한 식
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+addBtn.addEventListener('click', add);
